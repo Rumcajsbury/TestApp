da5dd2c303b7a538127828643d8ac32d9fc41602
Krzysiek Radwański <krzysztof.grzegorz.radwanski@gmail.com>
2020-01-25 17:25:04 +0100

    Adding Automated Login to App

---
+++ eShopOnWeb.UITests/PagesModels/HomePage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System.Linq;
+
+namespace eShopOnWeb.UITests.PagesModels
+{
+    public class HomePage
+    {
+        private IWebDriver _driver;
+        private string HomePageUrl = $"https://localhost:44315/";
+
+        public HomePage(IWebDriver driver) => _driver = driver;
+        public IWebElement LoginBtn => _driver.FindElements(By.ClassName("esh-identity-name")).First(x => x.Text.Equals("LOGIN"));
+
+        public void gotToPage() => _driver.Navigate().GoToUrl(HomePageUrl);
+        public LoginPage goToLoginPage()
+        {
+            LoginBtn.Click();
+            return new LoginPage(_driver);
+        }
+
+    }
+}
