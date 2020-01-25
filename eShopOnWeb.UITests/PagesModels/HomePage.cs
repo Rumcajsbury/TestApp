0eb5e009f49fdbc690de9bd687904d6f4aacc473
Krzysiek Radwański <krzysztof.grzegorz.radwanski@gmail.com>
2020-01-25 19:44:11 +0100

    Adding First scenario to UI tests

--- eShopOnWeb.UITests/PagesModels/HomePage.cs
+++ eShopOnWeb.UITests/PagesModels/HomePage.cs
@@ -9,7 +9,7 @@ namespace eShopOnWeb.UITests.PagesModels
         private string HomePageUrl = $"https://localhost:44315/";
 
         public HomePage(IWebDriver driver) => _driver = driver;
-        public IWebElement LoginBtn => _driver.FindElements(By.ClassName("esh-identity-name")).First(x => x.Text.Equals("LOGIN"));
+        public IWebElement LoginBtn => _driver.FindElements(By.ClassName("esh-identity-name")).FirstOrDefault(x => x.Text.Equals("LOGIN"));
 
         public void gotToPage() => _driver.Navigate().GoToUrl(HomePageUrl);
         public LoginPage goToLoginPage()
--- eShopOnWeb.UITests/PagesModels/LoginPage.cs
+++ eShopOnWeb.UITests/PagesModels/LoginPage.cs
@@ -12,12 +12,17 @@ namespace eShopOnWeb.UITests.PagesModels
         private IWebElement LoginInput => _driver.FindElements(By.ClassName("form-control")).First(x => x.GetAttribute("type") == "email");
         private IWebElement PasswordInput => _driver.FindElements(By.ClassName("form-control")).First(x => x.GetAttribute("type") == "password");
         private IWebElement LoginBtn => _driver.FindElements(By.ClassName("btn")).First(x => x.Text == "Log in");
+        private IWebElement NotLogedInAlert => _driver.FindElements(By.ClassName("validation-summary-errors")).First(x => x.Text == "Invalid login attempt.");
 
-        public void LogIntoApp()
+        public HomePage LogIntoApp(string password = "")
         {
             LoginInput.SendKeys(LogonUser.Login);
-            PasswordInput.SendKeys(LogonUser.Password);
+            PasswordInput.SendKeys(LogonUser.Password + password);
             LoginBtn.Click();
+
+            return new HomePage(_driver);
         }
+
+        public string GetNotLogedInAlert => NotLogedInAlert.Text;
     }
 }
