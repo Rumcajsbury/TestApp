7207e793c28660cabe47ce669906bc54084e49b2
Paweł Pindel <pawel.pindel@netsharpdev.com>
2020-01-29 20:29:43 +0100

    Start basket service testing

---
+++ eShopOnWeb.UnitTests/Services/BasketSerevice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace eShopOnWeb.UnitTests.Services
+{
+    [TestFixture]
+    public class BasketServiceTests
+    {
+        [Test]
+        public async Task AddItemToBasket_Should_ThrowNullReferenceException_WhenBasketIsNull()
+        {
+            var basketRepoMock = new Mock<IAsyncRepository<Basket>>();
+            var loggerMock = new Mock<IAppLogger<BasketService>>();
+            basketRepoMock.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((Basket) null);
+            var basketService = new BasketService(basketRepoMock.Object, loggerMock.Object);
+
+            var act = new Func<Task>(() => basketService.AddItemToBasket(
+                It.IsAny<int>(),
+                It.IsAny<int>(),
+                It.IsAny<int>(),
+                It.IsAny<int>()));
+
+            act.Should()
+                .ThrowAsync<NullReferenceException>();
+        }
+    }
+}
