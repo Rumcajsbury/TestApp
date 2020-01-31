763e98faf9044b5951202dd7e2bca5e5cadc6aaa
Krzysiek Radwański <krzysztof.grzegorz.radwanski@gmail.com>
2020-02-01 00:59:17 +0100

    Adding test for BasketExceptions

---
+++ eShopOnWeb.UnitTests/Exceptions/BasketNotFoundExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.GuardClauses;
+using FluentAssertions;
+using Microsoft.eShopWeb.ApplicationCore.Exceptions;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace eShopOnWeb.UnitTests.Exceptions
+{
+    [TestFixture]
+    public class BasketNotFoundExceptionTests
+    {
+        [Test]
+        public void BasketNotFoundException_MessageShouldBeGood()
+        {
+            var basketId = 11;
+
+            var basketException = new BasketNotFoundException(basketId);
+
+            basketException.Message.Should().Be($"No basket found with id {basketId}");
+        }
+
+        [Test]
+        public void NullBasket_ShouldThrow_BasketNotFoundException()
+        {
+            var guardClause = new Mock<IGuardClause>();
+
+            Action act = () => guardClause.Object.NullBasket(11, null);
+
+            act.Should()
+                .Throw<BasketNotFoundException>()
+                .WithMessage("No basket found with id 11");
+        }
+    }
+}
